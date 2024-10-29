6c40dbf5ad0527c9122bf35bf8f44d2bcf1a300c
HugoW5 <hugo05.wakt@gmail.com>
2024-10-29 14:46:41 +0100

    Added Graphics Card

---
+++ GraphicCard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GruppÖvning
+{
+	internal class GraphicCard
+	{
+		public string Name { get; set; }
+
+		public GraphicCard(string initalName)
+		{
+			Name = initalName;
+		}
+
+	}
+}

f14188d12b47bb238466c1fb35c6dba9f5028e0e
HugoW5 <hugo05.wakt@gmail.com>
2024-10-29 15:01:32 +0100

    Added GameConsole

---
+++ GameConsole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GruppÖvning
+{
+	internal class GameConsole
+	{
+		public Processor ConsoleProcessor { get; set; }
+		public GraphicCard ConsoleGraphicCard { get; set; }
+
+		public GameConsole(Processor initialProcessor, GraphicCard initalGraphicCard)
+		{
+			ConsoleProcessor = initialProcessor;
+			ConsoleGraphicCard = initalGraphicCard;
+		}
+	}
+}
